928a119f2e8c1bf3e2746e9b67f5043f652ec225
Jonathan Wong <jonathan.wong@tealium.com>
2018-03-21 15:11:41 -0700

    Fixed header.  Updated zip.

--- Source/TealiumCrashReporteriOS.h
+++ Source/TealiumCrashReporteriOS.h
@@ -5,13 +5,5 @@
 //  Created by Jonathan Wong on 2/27/18.
 //
 
-//! Project version number for TealiumCrashReporteriOS.
-FOUNDATION_EXPORT double TealiumCrashReporteriOSVersionNumber;
-
-//! Project version string for TealiumCrashReporteriOS.
-FOUNDATION_EXPORT const unsigned char TealiumCrashReporteriOSVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <TealiumCrashReporteriOS/PublicHeader.h>
-
 #import "CrashReporter.h"
 
