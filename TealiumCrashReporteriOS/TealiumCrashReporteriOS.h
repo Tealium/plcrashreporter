b3d2c412401b44b06c000a18996d992e5f8b757b
craigrouse <craig.rouse@gmail.com>
2019-10-16 13:17:06 +0100

    working Tealium scheme

---
+++ TealiumCrashReporteriOS/TealiumCrashReporteriOS.h
@@ -0,0 +1,18 @@
+//
+//  TealiumCrashReporteriOS.h
+//  TealiumCrashReporteriOS
+//
+//  Created by Craig Rouse on 15/10/2019.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TealiumCrashReporteriOS.
+FOUNDATION_EXPORT double TealiumCrashReporteriOSVersionNumber;
+
+//! Project version string for TealiumCrashReporteriOS.
+FOUNDATION_EXPORT const unsigned char TealiumCrashReporteriOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TealiumCrashReporteriOS/PublicHeader.h>
+
+
